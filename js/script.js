fcdccd207bdd8d637fdc237fb1a014f3a66d4f89
YashPadosect <84671672+YashPadosect@users.noreply.github.com>
2022-06-20 21:41:50 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+//     $('#role').html('web designer').delay(10).html('graphic designer')
+//         .delay(10).html('UX developer');
+// });
+
+//Replace Text function		 
+$(function() {
+    count = 0;
+    wordsArray = ["while Travelling", "while Shopping", "for Food", "for Entertainment", "for Literally Anything!"];
+    setInterval(function() {
+        count++;
+        $("#phrase").fadeOut(500, function() {
+            $(this).text(wordsArray[count % wordsArray.length]).fadeIn(500);
+        });
+    }, 5000);
+});
+//End Replace Text function
